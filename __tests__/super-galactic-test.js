c7879ec0a23018a445ae3d2e13a2f206b5e8f592
Christen Weston <paleeserecycle@gmail.com>
2021-12-28 15:41:21 -0800

    Test written for venusAge() with a good fail

--- __tests__/super-galactic-test.js
+++ __tests__/super-galactic-test.js
@@ -15,3 +15,11 @@ describe('Person.prototype.mercuryAge', () => {
     expect(newPerson.mercuryAge()).toEqual(150);
   });
 });
+
+describe('Person.prototype.venusAge', () => {
+  
+  test('Should return 58 if 36 is supplied as the earth age for the person', () => {
+    const newPerson = new Person(36);
+    expect(newPerson.venusAge()).toEqual(58);
+  });
+});
