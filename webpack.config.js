a693b2b3376ff522b7e3b32b773617b92b3384f0
Christen Weston <paleeserecycle@gmail.com>
2022-01-01 14:39:22 -0800

    Added in galaxy picture and webpack.config to have url loader

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,10 @@ module.exports = {
         ]
       },
       {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader?limit=100000'
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "eslint-loader"
