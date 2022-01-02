dbafec3d429e99d90133f2ed577358608901ec65
Christen Weston <paleeserecycle@gmail.com>
2022-01-01 16:32:14 -0800

    Updated page so results show after submit and form hides

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-//import $ from "jquery";
+import $ from "jquery";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-//import Person from './super-galactic.js';
+import Person from './super-galactic.js';
+
+$(document).ready(function() {
+  $("form#galactic-form").submit(function(event) {
+    event.preventDefault();
+    let earthAge = $("select#earth-age").val();
+    let sex = $("select#sex").val();
+
+    let newPerson = new Person(earthAge, sex);
+
+    $(".galactic-quiz").hide();
+    $(".galactic-results").show();
+  })
+})
