0dc5ee54860320dbf2b21e3dff28d206ccf883fc
Christen Weston <paleeserecycle@gmail.com>
2022-01-02 16:25:31 -0800

    Updated UI to show correction expectancy section depending on years left

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Person from './super-galactic.js';
 
+Person.prototype.displayLifeExpectancy = function() {
+  if (this.pastLifeExpectancy === true) {
+    $(".years-past-life-expectancy").show();
+  }
+  else if (this.pastLifeExpectancy === false) {
+    $(".years-left").show();
+  }
+}
 
 $(document).ready(function() {
   $("form#galactic-form").submit(function(event) {
@@ -17,12 +25,13 @@ $(document).ready(function() {
     let marsAge = newPerson.marsAge();
     let jupiterAge = newPerson.jupiterAge();
     let yearsLeft = parseInt(newPerson.yearsLeftToLive());
+    newPerson.displayLifeExpectancy();
 
     $(".mercury-age").html(mercuryAge);
     $(".venus-age").html(venusAge);
     $(".mars-age").html(marsAge);
     $(".jupiter-age").html(jupiterAge);
-    $(".years-left").html(yearsLeft);
+    $(".years-remaining").html(yearsLeft);
     $(".galactic-quiz").hide();
     $(".galactic-results").show();
   });
